e87461ecd33ce5f1be73c66d4c95386c93b8e9a8
unknown <adrianteranp86@gmail.com>
2020-12-10 00:44:29 -0300

    Creando Modelos

--- starwarsLFEC/catalogo/admin.py
+++ starwarsLFEC/catalogo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Jugete , Usuario
+
+admin.site.register(Jugete)
+admin.site.register(Usuario)
--- starwarsLFEC/catalogo/models.py
+++ starwarsLFEC/catalogo/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.urls import reverse # Se utilizaran para direccionar los path de nuestro  proyecto  asociado.
+import uuid                     # Se utlizara para relacionar los objetos.
+
 
 # Create your models here.
+ 
+class Jugete(models.Model):
+     idJugete = models.UUIDField(primary_key=True, default=uuid.uuid4)
+     nombreJugete = models.CharField(max_length=200)
+     marca = models.CharField(max_length=200)
+     precio = models.TextField(max_length=1000)
+     stock = models.URLField(max_length=100, default='')
+    
+     def get_absolute_url(self):
+		    return reverse('jugete-detail', args=[str(self.idJugete)])
+
+     def __str__(self):
+		    return self.nombreJugete
+		
+
+
+class Usuario(models.Model):
+     idUsu = models.UUIDField(primary_key=True, default=uuid.uuid4)
+     nombreUsu = models.CharField(max_length=200)
+     apellido = models.CharField(max_length=200)
+     pais = models.TextField(max_length=1000)
+     mail = models.URLField(max_length=100, default='')
+    
+     def __str__(self):
+        
+          return self.nombreUsu
+    
+     def get_absolute_url(self):
+        
+          return reverse('Usario-detail', args=[str(self.idUsu)])
+
+
